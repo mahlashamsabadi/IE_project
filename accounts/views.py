99f67b04475a8a4910ac488212adbab219b1113c
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2023-01-19 00:42:01 +0330

    showLog view added

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenViewBase
 from rest_framework_tracking.mixins import LoggingMixin
 from permissions import *
 from rest_framework import generics
+import sqlite3
+from sqlite3 import Error
+from rest_framework.permissions import IsAuthenticated , IsAdminUser
 
 class Register(LoggingMixin , generics.GenericAPIView):
     def post(self , request):
@@ -25,14 +28,55 @@ class CustomTokenObtainPairView(LoggingMixin, TokenObtainPairView):
     # Replace the serializer with your custom
     serializer_class = CustomTokenObtainPairSerializer
 
+
 class DhcpConfig(LoggingMixin , generics.GenericAPIView):
     permission_classes = [IsDhcpManager ,]
 
     def get(self, request):
         self.check_object_permissions(request , request.user)
         return Response("helloooo")
-    def handle_log(self):
-         print(self.log['user'])
-         return self.log['user']
 
+class ShowLogs(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated ,]
+    def get(self , request):
+        logs = getLogs(request.user)
+        data1 = []
+        for i in logs: 
+            data = {}
+            data['id'] = i[0]
+            data['requested_at'] = i[1]
+            data['response_ms'] = i[2]
+            data['path'] = i[3]
+            data['remote_addr'] = i[4]
+            data['host'] = i[5]
+            data['method'] = i[6]
+            data['query_params'] = i[7]
+            data['data'] = i[8]
+            data['response'] = i[9]
+            data['status_code'] = i[10]
+            data['user_id'] = i[11]
+            data['view'] = i[12]
+            data['view_method'] = i[13]
+            data['errors'] = i[14]
+            data['username_persistent'] = i[15]
+            data1.append(data)
+        return Response(data1)
         
+        
+def getLogs(user):
+
+    database = r"db.sqlite3"
+    connection = None
+    try:
+        connection = sqlite3.connect(database)
+    except Error as e:
+        print(e)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM rest_framework_tracking_apirequestlog")
+    userLogs = []
+    logs = cursor.fetchall()
+    for i in logs:
+        if (i[15] == user.username):
+            userLogs.append(i)
+    return userLogs
+   
