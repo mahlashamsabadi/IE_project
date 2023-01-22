5a13537a990e54c11ea00119c6523e6a218b9f7a
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2023-01-23 00:57:21 +0330

    Update for server

--- server_config/Dhcp_config.py
+++ server_config/Dhcp_config.py
@@ -3,22 +3,24 @@ import subprocess
 import json
 
 return_data = {}
-pwd = "fatemeh"
+pwd = "mahla_sh"
 
 #Start Dhcp
-cmd_start = "sudo /etc/init.d/dhcp start"
+cmd_start = "sudo service isc-dhcp-server start"
 output_start = subprocess.run('echo {} | sudo -S {}'.format(pwd, cmd_start), shell=True, capture_output=True, text=True)
 print("output is", output_start.stdout, "error is",output_start.stderr)
 
 
 #Stop Dhcp
-cmd_stop = "sudo /etc/init.d/dhcp stop"
+cmd_stop = "sudo sudo service isc-dhcp-server stop"
 output_stop = subprocess.run('echo {} | sudo -S {}'.format(pwd, cmd_stop), shell=True, capture_output=True, text=True)
 print("output is", output_stop.stdout, "error is",output_stop.stderr)
 
 
 #Status
-output_status = subprocess.run('systemctl status dhcp', shell=True, capture_output=True, text=True)
+output_status = subprocess.run('sudo service isc-dhcp-server status', shell=True, capture_output=True, text=True)
 print("output is", output_status.stdout, "error is",output_status.stderr)
 
 
+#/etc/dhcp/dhcpd.conf
+#Change IP Range
