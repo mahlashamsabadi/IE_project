3b9df149bedfce97082afbb7934dcab594d27265
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2023-01-23 14:24:31 +0330

    view for WebServerConfigStatus

--- accounts/views.py
+++ accounts/views.py
@@ -113,15 +113,15 @@ class DhcpConfigStatus(LoggingMixin , generics.GenericAPIView):
 
 
 
-            PID_s = "" #؟؟
+            Pid_s = "" #؟؟
 
             index = s.find("Main PID: ") + 10
             while s[index]!= " " :
-                PID_s += s[index]
+                Pid_s += s[index]
                 index = index+1
 
 
-            return_data["Main PID"] =PID_s
+            return_data["Main PID"] =Pid_s
 
         return_data = json.dumps(return_data, indent = 4)
         return Response(return_data)
@@ -182,8 +182,9 @@ class MailConfigStatus(LoggingMixin , generics.GenericAPIView):
 
         return_data["statusError"] = output.stderr
 
+        s_out = output.stdout
         
-        if  output.stdout != "":
+        if s_out != "":
 
             s = output.stdout
             load_s = ""
@@ -216,25 +217,25 @@ class MailConfigStatus(LoggingMixin , generics.GenericAPIView):
 
 
 
-            PID_s = ""
+            Pid_s = ""
 
             index = s.find("Main PID: ") + 10
             while s[index]!= " " :
-                 PID_s += s[index]
+                 Pid_s += s[index]
                  index = index+1
 
 
-            return_data["Main PID"] =PID_s
+            return_data["Main PID"] =Pid_s
 
-            CPU_s = ""
+            cpu_s = ""
 
             index = s.find("CPU: ") + 5
             while s[index]!= "\n" :
-                 CPU_s += s[index]
+                 cpu_s += s[index]
                  index = index+1
 
 
-            return_data["CPU"] =CPU_s
+            return_data["CPU"] =cpu_s
 
         return_data = json.dumps(return_data, indent = 4)
         return Response(return_data)
@@ -267,6 +268,113 @@ class WebServerConfigStop(LoggingMixin , generics.GenericAPIView):
         output = subprocess.run('echo {} | sudo -S {}'.format(pwd, cmd_start), shell=True, capture_output=True, text=True)
 
         return_data["stopError"] = output.stderr
+        return_data = json.dumps(return_data, indent = 4)
+
+        return Response(return_data)
+
+
+class WebServerConfigStatus(LoggingMixin , generics.GenericAPIView):
+    permission_classes = [IsWebManager,]
+
+    def get(self, request):
+        self.check_object_permissions(request , request.user)
+        return_data = {}
+        #change password
+        pwd = "mahla_sh"
+        cmd_status = "systemctl status nginx"
+        output = subprocess.run(cmd_status, shell=True, capture_output=True, text=True)
+
+        return_data["statusError"] = output.stderr
+        s_out = output.stdout
+
+        if s_out != "":
+
+            s = output.stdout
+            load_s = ""
+
+            index = s.find("Loaded: ") + 8
+            while s[index]!= " " :
+                 load_s += s[index]
+                 index = index + 1
+
+            return_data["Loaded"] = load_s
+
+            active_s = ""
+
+            index = s.find("Active: ") + 8
+            while s[index]!= " " :
+                 active_s += s[index]
+                 index = index + 1
+
+            return_data["Active"] = active_s
+
+            process_s = ""
+
+            index = s.find("Process: ") + 9
+            while s[index]!= " " :
+                 process_s += s[index]
+                 index = index+1
+
+
+            return_data["Process"] = process_s
+
+
+
+            Pid_s = ""
+
+            index = s.find("Main PID: ") + 10
+            while s[index]!= " " :
+                 Pid_s += s[index]
+                 index = index + 1
+
+
+            return_data["Main PID"] = Pid_s
+
+            if active_s == "active":
+                index = s.find("Tasks: ") + 7
+                Tasks_s = ""
+
+                while s[index] != " ":
+
+                    Tasks_s += s[index]
+                    index = index + 1
+
+                return_data["Tasks"] = Tasks_s
+
+
+                index = s.find("limit: ") + 7
+                Task_limit = ""
+
+                while s[index] != ")":
+                    Task_limit += s[index]
+                    index += 1 
+
+                return_data["Task_limit"] = Task_limit
+
+
+                Memory_s = ""
+                index = s.find("Memory: ") + 8
+
+                while s[index] != "\n":
+
+                    Memory_s += s[index]
+                    index += 1
+
+                return_data["Memory"] = Memory_s
+
+
+            cpu_s = ""
+
+            index = s.find("CPU: ") + 5
+            while s[index]!= "\n" :
+                 cpu_s += s[index]
+                 index = index + 1
+
+
+            return_data["CPU"] = cpu_s
+
+
+
         return_data = json.dumps(return_data, indent = 4)
 
 
