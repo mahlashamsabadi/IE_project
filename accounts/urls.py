0448dbf05d71a03cc15a237e4c6f11b7870195c7
unknown <ashrafi.zahra81@gmail.com>
2023-01-27 05:32:05 -0800

    add change home directory and user mail urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,11 +18,14 @@ urlpatterns = [
     path('mail/start/', views.MailConfigStart.as_view(), name='mail_start'),
     path('mail/stop/', views.MailConfigStop.as_view(), name='mail_stop'),
     path('mail/status/', views.MailConfigStatus.as_view(), name='mail_status'),
+    path('mail/addUser/', views.AddUserMail.as_view(), name='add_user'),
     #Web Server Configuration
     path('web/start/', views.WebServerConfigStart.as_view(), name='web_start'),
     path('web/stop/', views.WebServerConfigStop.as_view(), name='web_stop'),
     path('web/status/', views.WebServerConfigStatus.as_view(), name='web_status'),
     path('ConfigGetHomeDir/', views.WebServerConfigGetHomeDir.as_view(), name='web_get_home_dir'),
+    path('ConfigChangeHomeDir/', views..WebServerConfigChangeHomeDir.as_view(), name='web_change_home_dir'),
+
 
 
     
